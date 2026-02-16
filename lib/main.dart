239fc30a63a8176f1743c7c242ccf268179f200f
Daniele Coppotelli <daniele.coppotelli@msccrociere.it>
2026-02-16 17:30:27 +0100

    test deploy

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/vessel_provider.dart';
 import 'screens/map_screen.dart';
-
+//test
 void main() {
   runApp(const MyApp());
 }
